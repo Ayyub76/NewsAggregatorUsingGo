f2a18c4dfc10a5ff0a22608736c2d3af58dcc646
Ayyub76 <ayyub3855@gmail.com>
2019-11-07 20:37:33 +0530

    Go formatting

--- src/web.go
+++ src/web.go
@@ -1,35 +1,36 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"net/http"
 	"html/template"
 	"io/ioutil"
-	"encoding/xml"
+	"net/http"
 	"strings"
+	"sync"
 )
 
+var wg sync.WaitGroup
 
 type SiteMapIndex struct {
-	Locations [] string `xml:"sitemap>loc"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
 type News struct {
-	Titles [] string `xml:"url>news>title"`
-	Keywords [] string `xml:"url>news>keywords"`
-	Locations [] string `xml:"url>loc"`
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 type NewsMap struct {
-	Keyword string
+	Keyword  string
 	Location string
 }
 
 type NewsAggPage struct {
 	Title string
-	News map[string]NewsMap
+	News  map[string]NewsMap
 }
-
 
 func main() {
 	http.HandleFunc("/", index_handler)
@@ -38,8 +39,8 @@ func main() {
 }
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Test Web!!</h1>\n")
-	fmt.Fprintf(w, "<p1>This is a test webpage made using golang!!!</p1>\n")
+	fmt.Fprintf(w, "<h1>News Aggregator!!</h1>\n")
+	fmt.Fprintf(w, "<p1>News aggregator for washingtonpost</p1>\n")
 }
 
 func news_agg_handler(w http.ResponseWriter, r *http.Request) {
@@ -49,30 +50,27 @@ func news_agg_handler(w http.ResponseWriter, r *http.Request) {
 	News_map := make(map[string]NewsMap)
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	
 	xml.Unmarshal(bytes, &s)
-
-	//fmt.Println(s)
+	resp.Body.Close()
 
 	for _, val := range s.Locations {
 		p = strings.Trim(val, "\n")
 		resp1, _ := http.Get(p)
 		bytes1, _ := ioutil.ReadAll(resp1.Body)
 		xml.Unmarshal(bytes1, &n)
+		resp1.Body.Close()
 
 		for ind, _ := range n.Keywords {
 			News_map[n.Titles[ind]] = NewsMap{n.Keywords[ind], n.Locations[ind]}
-			//fmt.Println(ind1, n.Titles[ind])
 		}
-		resp1.Body.Close()
+
 	}
-	resp.Body.Close()
 
 	page := NewsAggPage{Title: "Sample title", News: News_map}
 	t, err := template.ParseFiles("sampletemplate.html")
 	if err == nil {
 		fmt.Println(t.Execute(w, page))
-    } else {
-    	fmt.Println(err)
-    }
+	} else {
+		fmt.Println(err)
+	}
 }
