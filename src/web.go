66c1f40c78a0059d51807499b5ec0a019882d6b7
Ayyub76 <ayyub3855@gmail.com>
2019-11-09 12:34:42 +0530

    Added Concurrency

--- src/web.go
+++ src/web.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type SiteMapIndex struct {
 	Locations []string `xml:"sitemap>loc"`
@@ -40,34 +43,43 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<p1>News aggregator for washingtonpost</p1>\n")
 }
 
+func news_routine(c chan News, loc string){
+	defer wg.Done()
+	var n News
+	p := strings.Trim(loc, "\n")
+	resp1, _ := http.Get(p)
+	bytes1, _ := ioutil.ReadAll(resp1.Body)
+	xml.Unmarshal(bytes1, &n)
+	resp1.Body.Close()
+	fmt.Println("Added:", p)
+	c <- n
+}
+
 func news_agg_handler(w http.ResponseWriter, r *http.Request) {
 	var s SiteMapIndex
-	var n News
-	var p string
 	News_map := make(map[string]NewsMap)
+	queue := make(chan News, 100)
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 	resp.Body.Close()
 
 	for _, val := range s.Locations {
-		p = strings.Trim(val, "\n")
-		resp1, _ := http.Get(p)
-		bytes1, _ := ioutil.ReadAll(resp1.Body)
-		xml.Unmarshal(bytes1, &n)
-		resp1.Body.Close()
-
-		for ind, _ := range n.Keywords {
-			News_map[n.Titles[ind]] = NewsMap{n.Keywords[ind], n.Locations[ind]}
+		wg.Add(1)
+		go news_routine(queue, val)
+	}
+	wg.Wait()
+	close(queue)
+	for ele := range queue {
+		for ind, _ := range ele.Keywords {
+			News_map[ele.Titles[ind]] = NewsMap{ele.Keywords[ind], ele.Locations[ind]}
 		}
-
 	}
 
 	page := NewsAggPage{Title: "Sample title", News: News_map}
 	t, err := template.ParseFiles("sampletemplate.html")
-	if err == nil {
-		fmt.Println(t.Execute(w, page))
-	} else {
+	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(t.Execute(w, page))
 }
